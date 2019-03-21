c41c2e331b16558cfc661990d6db9774b8fbbd8b
Ashley <36495929+myashley@users.noreply.github.com>
2019-03-21 11:40:42 +1100

    Completed tutorial challenge

--- app.js
+++ app.js
@@ -8,3 +8,96 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer, gamePlaying
+
+init()
+
+// ROLL RICE button
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    if (gamePlaying) {
+        // 1. Random number
+        let dice = Math.floor(Math.random() * 6) + 1
+
+        // 2. Display the result
+        const diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block'
+        diceDOM.src = 'dice-' + dice + '.png'
+
+        // 3. Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            // Add score
+            roundScore += dice
+            document.querySelector('#current-' + activePlayer).textContent = roundScore
+        } else {
+            // Next player
+            nextPlayer()
+        }
+    }
+
+})
+
+
+// HOLD button
+document.querySelector('.btn-hold').addEventListener('click', function() {
+
+    if (gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore
+        
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+
+        // Check if player won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+            document.querySelector('.dice').style.display = 'none'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            gamePlaying = false
+        } else {
+            // Next player
+            nextPlayer()
+        }
+    }
+})
+
+
+// NEW GAME button
+document.querySelector('.btn-new').addEventListener('click', init)
+
+
+
+// Functions
+function init(){
+    scores = [0, 0]
+    activePlayer = 0
+    roundScore = 0
+    gamePlaying = true
+
+    document.querySelector('.dice').style.display = 'none'
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner') 
+    document.querySelector('.player-0-panel').classList.add('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+}
+
+function nextPlayer(){
+    activePlayer  === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+    document.querySelector('.dice').style.display = 'none'
+}
+
+
