8d6da91c8c17c0c643a94bc4052b92987e583fad
Ashley <36495929+myashley@users.noreply.github.com>
2019-04-11 11:37:28 +1000

    Finish challenge 3

--- challenges.js
+++ challenges.js
@@ -16,7 +16,8 @@ Extended Challenges:
 2. Add an input field to the HTML where players can set the winning score, 
 so that they can change the predefined score of 100. 
 
-3. 
+3. Add another dice to the game, so that there are two dices now. 
+The player looses his current score when one of them is a 1.
 
 */
 
@@ -31,30 +32,43 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     
     if (gamePlaying) {
         // 1. Random number
-        let dice = Math.floor(Math.random() * 6) + 1
+        let dice1 = Math.floor(Math.random() * 6) + 1
+        let dice2 = Math.floor(Math.random() * 6) + 1
 
         // 2. Display the result
-        const diceDOM = document.querySelector('.dice')
-        diceDOM.style.display = 'block'
-        diceDOM.src = 'dice-' + dice + '.png'
+        
+        document.getElementById('dice-1').style.display = 'block'
+        document.getElementById('dice-2').style.display = 'block'
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png'
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png'
 
         // 3. Update the round score IF the rolled number was NOT a 1
-        if (dice === 6 && lastDice === 6) {
-            // Player looses score
-            scores[activePlayer] = 0
-            document.querySelector('#score-' + activePlayer).textContent = 0
-            nextPlayer()
-
-        } else if (dice !== 1) {
+        if (dice1 !== 1 && dice2 !== 1) {
             // Add score
-            roundScore += dice
+            roundScore += dice1 + dice2
             document.querySelector('#current-' + activePlayer).textContent = roundScore
         } else {
             // Next player
             nextPlayer()
         }
 
-        lastDice = dice
+        // 3. Update the round score IF the rolled number was NOT a 1
+        // if (dice === 6 && lastDice === 6) {
+        //     // Player looses score
+        //     scores[activePlayer] = 0
+        //     document.querySelector('#score-' + activePlayer).textContent = 0
+        //     nextPlayer()
+
+        // } else if (dice !== 1) {
+        //     // Add score
+        //     roundScore += dice
+        //     document.querySelector('#current-' + activePlayer).textContent = roundScore
+        // } else {
+        //     // Next player
+        //     nextPlayer()
+        // }
+
+        // lastDice = dice
     }
 
 })
@@ -84,7 +98,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // Check if player won the game
         if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
-            document.querySelector('.dice').style.display = 'none'
+            hideDice()
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
             gamePlaying = false
@@ -108,7 +122,7 @@ function init(){
     roundScore = 0
     gamePlaying = true
 
-    document.querySelector('.dice').style.display = 'none'
+    hideDice()
     document.getElementById('score-0').textContent = '0'
     document.getElementById('score-1').textContent = '0'
     document.getElementById('current-0').textContent = '0'
@@ -129,6 +143,10 @@ function nextPlayer(){
     document.getElementById('current-1').textContent = '0'
     document.querySelector('.player-0-panel').classList.toggle('active')
     document.querySelector('.player-1-panel').classList.toggle('active')
-    document.querySelector('.dice').style.display = 'none'
+    hideDice()
 }
 
+function hideDice() {
+    document.getElementById('dice-1').style.display = 'none'
+    document.getElementById('dice-2').style.display = 'none'
+}
